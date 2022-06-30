8bf8a050f5f10efe3cf0d5d190b15334ab1f45fd
Marcus Ferreira <mv.ferreirapinto@gmail.com>
2022-06-30 02:22:21 -0300

    add comments

--- love-template/main.lua
+++ love-template/main.lua
@@ -1,14 +1,15 @@
 function love.load()
-	love.graphics.setDefaultFilter("nearest", "nearest")
-	require("src.Dependencies")
-	math.randomseed(os.time())
-	StateMachine:change(TestState)
+	love.graphics.setDefaultFilter("nearest", "nearest")	-- Set pixel filter
+	require("src.dependencies")								-- Import all
+	math.randomseed(os.time())								-- Generate RNG
+	StateMachine:change(TestState)							-- Change to first state
 end
 
 function love.update(dt)
-	StateMachine.current:update(dt)
+	StateMachine.current:update(dt)							-- Update the current state
 end
 
 function love.draw()
-	StateMachine.current:draw()
+	love.graphics.scale(WINDOW_SCALE, WINDOW_SCALE)			-- Scale the window to match the virtual dimensions
+	StateMachine.current:draw()								-- Draw the current state
 end
