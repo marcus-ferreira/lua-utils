9c89528c57e5f122525a83b66b01e697ddc05127
Marcus Ferreira <mv.ferreirapinto@gmail.com>
2022-06-30 02:22:31 -0300

    remove recursion

--- love-template/src/Dependencies.lua
+++ love-template/src/Dependencies.lua
@@ -1,15 +1,6 @@
--- Import src
-for key, file in pairs(love.filesystem.getDirectoryItems("src")) do
-	if file ~= "states" and file ~= "Dependencies.lua" then
-		require("src." .. string.gsub(file, ".lua", ""))
-	end
-end
+require("src.constants")
+require("src.globals")
+require("src.state_machine")
 
--- Import states
-require("src.states._Base")
-for key, file in pairs(love.filesystem.getDirectoryItems("src/states")) do
-	if file ~= "_Base.lua" then
-		require("src.states." .. string.gsub(file, ".lua", ""))
-	end
-end
-
+require("src.states._base_state")
+require("src.states.test_state")
