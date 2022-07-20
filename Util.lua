71cb301c745775667f46651bf2a35e4bd61fdeed
Marcus Ferreira <mv.ferreirapinto@gmail.com>
2022-07-20 04:37:11 -0300

    Remove isColliding and newQuads

--- Util.lua
+++ Util.lua
@@ -8,29 +8,3 @@ function table.slice(tbl, first, last)
 	for i = first or 1, last or #tbl do table.insert(sliced, tbl[i]) end
 	return sliced
 end
-
----Check if an object is colliding other with AABB detection
----@param a table # The first object
----@param b table # The second object
----@return boolean # Returns true if is colliding
-function IsColliding(a, b)
-	return a.x < b.x + b.width and
-		a.y < b.y + b.height and
-		b.x < a.x + a.width and
-		b.y < a.y + a.height
-end
-
----Creates a table of quads
----@param atlas love.Image
----@param tw number
----@param th number
-function love.graphics.newQuads(atlas, tw, th)
-	local quads = {}
-	for y = 0, atlas:getHeight() - 1, th do
-		for x = 0, atlas:getWidth() - 1, tw do
-			local tile = love.graphics.newQuad(x, y, tw, th, atlas:getDimensions())
-			table.insert(quads, tile)
-		end
-	end
-	return quads
-end
