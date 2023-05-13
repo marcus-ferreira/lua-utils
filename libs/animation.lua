5844f81e3567e50b15eb3adeefc69a0481ba4e96
Marcus Ferreira <mv.ferreirapinto@gmail.com>
2023-05-13 01:36:04 -0300

    Created grid and updated lib

--- libs/animation.lua
+++ libs/animation.lua
@@ -2,26 +2,52 @@
 	Animation library
 
 	Author: Marcus Ferreira
-	Version: 1.0
+	Version: 1.1
 ]]--
+
+---@class Grid
+Grid = {}
+
+---Creates a new Grid, a list of Quads.
+---@param image love.Image
+---@param tileWidth number
+---@param tileHeight number
+---@return Grid
+function Grid:new(image, tileWidth, tileHeight)
+	---@class Grid
+	local grid = {}
+	grid.image = image
+
+	for y = 0, grid.image:getHeight() - 1, tileHeight do
+		for x = 0, grid.image:getWidth() - 1, tileWidth do
+			local tile = love.graphics.newQuad(x, y, tileWidth, tileHeight, grid.image:getDimensions())
+			table.insert(grid, tile)
+		end
+	end
+
+	setmetatable(grid, { __index = self })
+	return grid
+end
 
 ---@class Animation
 Animation = {}
 
----Creates a new Animation
----@param frames table # A table of the numbers of the frames in a quad list
+---Creates a new Animation.
+---@param grid Grid # The Grid to be used.
+---@param frames table # A table of the numbers of the frames in a quad list.
 ---@param interval? number # The interval between frame quads, in seconds. The default value is the "interval" value of the class.
 ---@param loop? boolean # True if the animation should be looped or false if contrary. The default value is the "loop" value of the class.
 ---@return Animation animation # The new Animation object
-function Animation:new(frames, interval, loop)
+function Animation:new(grid, frames, interval, loop)
 	---@class Animation
-	local animation = {
-		frames = frames,
-		interval = interval or 1,
-		loop = loop or false,
-		timer = 0,
-		currentFrame = 1,
-	}
+	local animation = {}
+	animation.grid = grid
+	animation.frames = frames
+	animation.indexCurrentFrame = 1
+	animation.interval = interval or 1
+	animation.loop = loop or false
+	animation.timer = 0
+
 	setmetatable(animation, { __index = self })
 	return animation
 end
@@ -29,38 +55,50 @@ end
 ---Updates the Animation
 ---@param dt number
 function Animation:update(dt)
+	-- don't update if there's only 1 frame
 	if #self.frames == 1 then return end
 
+	-- increases timer over time
 	self.timer = self.timer + dt
 
+	-- change to next frame and reset timer
 	if self.timer > self.interval then
-		self.currentFrame = self.currentFrame + 1
+		self.indexCurrentFrame = self.indexCurrentFrame + 1
 		self.timer = self.timer - self.interval
 	end
 
-	if self.currentFrame > #self.frames then
+	-- go back to first frame
+	if self.indexCurrentFrame > #self.frames then
 		if self.loop then
-			self.currentFrame = 1
+			self.indexCurrentFrame = 1
 		else
-			self.currentFrame = #self.frames
+			self.indexCurrentFrame = #self.frames
 		end
 	end
 end
 
+function Animation:draw(x, y)
+	love.graphics.draw(self.grid.image, self.grid[self:getCurrentFrame()], x, y)
+end
+
 ---Gets the current frame of the animation (not the index)
----@return number # The frame of the animation
+---@return love.Quad # The frame of the animation
 function Animation:getCurrentFrame()
-	return self.frames[self.currentFrame]
+	return self.frames[self.indexCurrentFrame]
 end
 
 ---Checks if the animation has ended
 ---@return boolean # True if the last frame of the animation is playing
 function Animation:isEnded()
-	return self.currentFrame == #self.frames
+	return self.indexCurrentFrame == #self.frames
 end
 
 ---Sets the animation to loop or not
 ---@param boolean boolean # True to turn loop on, false to turn off
 function Animation:setLoop(boolean)
 	self.loop = boolean
+end
+
+function Animation:goToFrame(frameNumber)
+	self.indexCurrentFrame = frameNumber
 end
