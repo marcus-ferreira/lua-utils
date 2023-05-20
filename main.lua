6ca29267243288cc383f51f7d1991bc23b51f1c7
Marcus Ferreira <mv.ferreirapinto@gmail.com>
2023-05-20 03:36:37 -0300

    Changed main

--- main.lua
+++ main.lua
@@ -1,83 +1,52 @@
-require("libs.collision")
+require("libs.shader")
 
-local solids = {}
-local player = {}
+function love.load()
+	love.graphics.setDefaultFilter("nearest", "nearest")
+	love.window.setMode(1200, 1000, {})
 
-for y = 4, 9 do
-	for x = 4, 8 do
-		table.insert(solids, {
-			x = (x - 1) * 32,
-			y = (y - 1) * 32,
-			width = 32,
-			height = 32,
-			color = { 1, 1, 1 },
-			update = function(self)
-				self.color = { 1, 1, 1 }
-				if IsColliding(self, player) then self.color = { 1, 0, 0 } end
-			end,
-			draw = function(self)
-				love.graphics.setColor(self.color)
-				love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
-			end
-		})
+	shaders = {}
+	for i, shader in ipairs(Shader) do
+		shaders[i] = love.graphics.newShader(shader)
+		print(shaders[i])
 	end
+	currentShader = 1
+	isTurnedOn = false
+
+	background = {}
+	-- background.img = love.graphics.newImage("resources/world.png")
+	background.img = love.graphics.newImage("resources/kanto.png")
+	background.x = 0
+	background.y = 0
 end
 
-player = {
-	x = 0,
-	y = 0,
-	width = 32,
-	height = 32,
-	color = { 0, 0, 1 },
-	isColliding = { x = 0, y = 0 },
-	update = function(self, dt)
-		if love.keyboard.isDown("a") and self.isColliding.x ~= -1 then self.x = self.x - 3 end
-		if love.keyboard.isDown("d") and self.isColliding.x ~= 1 then self.x = self.x + 3 end
-		if love.keyboard.isDown("w") and self.isColliding.y ~= -1 then self.y = self.y - 3 end
-		if love.keyboard.isDown("s") and self.isColliding.y ~= 1 then self.y = self.y + 3 end
-
-		for i, solid in ipairs(solids) do
-			if IsColliding(self, solid) then
-				if GetCollisionPosition(self, solid) == "top to bottom" then
-					self.isColliding.y = 1
-					self.y = solid.y - self.height
-				elseif GetCollisionPosition(self, solid) == "bottom to top" then
-					self.isColliding.y = -1
-					self.y = solid.y + solid.height
-				elseif GetCollisionPosition(self, solid) == "left to right" then
-					self.isColliding.x = 1
-					self.x = solid.x - self.width
-				elseif GetCollisionPosition(self, solid) == "right to left" then
-					self.isColliding.x = -1
-					self.x = solid.x + solid.width
-				end
-			else
-				self.isColliding = { x = 0, y = 0 }
-			end
-		end
-
-
-	end,
-	draw = function(self)
-		love.graphics.setColor(self.color)
-		love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
-		love.graphics.setColor(1, 1, 1)
-	end
-}
-
 function love.update(dt)
-	for i, solid in ipairs(solids) do
-		solid:update()
+	if currentShader == 1 then
+		shaders[currentShader]:send("targetX", love.mouse.getX())
+		shaders[currentShader]:send("targetY", love.mouse.getY())
+		shaders[currentShader]:send("radius", 200)
 	end
-	player:update(dt)
+
+	if love.keyboard.isDown("w") then background.y = background.y + 5 end
+	if love.keyboard.isDown("s") then background.y = background.y - 5 end
+	if love.keyboard.isDown("a") then background.x = background.x + 5 end
+	if love.keyboard.isDown("d") then background.x = background.x - 5 end
 end
 
 function love.draw()
-	for i, solid in ipairs(solids) do
-		solid:draw()
-		if IsColliding(player, solid) then
-			love.graphics.print(tostring(player.isColliding.x .. " " .. player.isColliding.y))
-		end
+	love.graphics.scale(2, 2)
+
+	if isTurnedOn then love.graphics.setShader(shaders[currentShader]) end
+
+	love.graphics.draw(background.img, background.x, background.y)
+
+	love.graphics.setShader()
+end
+
+function love.keypressed(key)
+	if key == "space" then
+		isTurnedOn = not isTurnedOn
+	elseif type(tonumber(key:sub(-1))) == "number" then
+		isTurnedOn = true
+		currentShader = tonumber(key:sub(-1))
 	end
-	player:draw()
 end
