860dfef1122581e677f4547530ee120083113b77
Marcus Ferreira <mv.ferreirapinto@gmail.com>
2023-05-24 17:52:12 -0300

    Added color lib

---
+++ libs/color.lua
@@ -0,0 +1,88 @@
+---@class color
+color = {}
+
+---Converts a hex (given without '#') value to RGB.
+---@param hex string
+---@return number red
+---@return number green
+---@return number blue
+function color.hexToRGB(hex)
+	local function convertHex(digit)
+		return digit
+			:gsub("a", 10)
+			:gsub("b", 11)
+			:gsub("c", 12)
+			:gsub("d", 13)
+			:gsub("e", 14)
+			:gsub("f", 15)
+	end
+
+	return
+		convertHex(hex:sub(1, 1)) * 16 + convertHex(hex:sub(2, 2)),
+		convertHex(hex:sub(3, 3)) * 16 + convertHex(hex:sub(4, 4)),
+		convertHex(hex:sub(5, 5)) * 16 + convertHex(hex:sub(6, 6))
+end
+
+---Converts HSL to RGB. (input and output range: 0 - 1)
+---@param h number
+---@param s number
+---@param l number
+---@param a number
+---@return number red
+---@return number green
+---@return number blue
+---@return number alpha
+function color.HSLToRGB(h, s, l, a)
+	if s <= 0 then return l, l, l, a end
+
+	h, s, l = h * 6, s, l
+	local c = (1 - math.abs(2 * l - 1)) * s
+	local x = (1 - math.abs(h % 2 - 1)) * c
+	local m, r, g, b = (l - 0.5 * c), 0, 0, 0
+	if h < 1 then
+		r, g, b = c, x, 0
+	elseif h < 2 then
+		r, g, b = x, c, 0
+	elseif h < 3 then
+		r, g, b = 0, c, x
+	elseif h < 4 then
+		r, g, b = 0, x, c
+	elseif h < 5 then
+		r, g, b = x, 0, c
+	else
+		r, g, b = c, 0, x
+	end
+
+	return r + m, g + m, b + m, a
+end
+
+---Converts HSV to RGB. (input and output range: 0 - 1)
+---@param h number
+---@param s number
+---@param v number
+---@return number red
+---@return number green
+---@return number blue
+function color.HSVToRGB(h, s, v)
+	if s <= 0 then return v, v, v end
+
+	h = h * 6
+	local c = v * s
+	local x = (1 - math.abs((h % 2) - 1)) * c
+	local m, r, g, b = (v - c), 0, 0, 0
+	if h < 1 then
+		r, g, b = c, x, 0
+	elseif h < 2 then
+		r, g, b = x, c, 0
+	elseif h < 3 then
+		r, g, b = 0, c, x
+	elseif h < 4 then
+		r, g, b = 0, x, c
+	elseif h < 5 then
+		r, g, b = x, 0, c
+	else
+		r, g, b = c, 0, x
+	end
+
+	return r + m, g + m, b + m
+end
