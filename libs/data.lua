5a7530bc726cbd1f787467167cc88091ac791518
Marcus Ferreira <mv.ferreirapinto@gmail.com>
2023-05-24 18:07:35 -0300

    Updated data lib

--- libs/data.lua
+++ libs/data.lua
@@ -1,16 +1,14 @@
----Save the actual game in a serialized data file
----@param file string # The filename where the game will be saved
----@param gameData table # The table of the data to be saved
-function SaveGame(file, gameData)
-	local data = table.serialize(gameData)
-	love.filesystem.write(file, data)
+require("libs.table")
+
+---@class data
+data = {}
+
+function data.save(fileName, gameData)
+	love.filesystem.write(fileName, table.serialize(gameData))
 end
 
----Load data
----@param file string # The filename to be loaded
----@return table | nil data # The table of tables of the data loaded
-function LoadGame(file)
-	local data = love.filesystem.read(file)
-	if data == nil then return end
-	return table.unserialize(data)
+function data.load(fileName)
+	local fileString = love.filesystem.read(fileName)
+	if fileString == nil then return end
+	return table.deserialize(fileString)
 end
