4f6f8c35597d3127e887493bd24665d4e5e46cea
Marcus Ferreira <mv.ferreirapinto@gmail.com>
2023-06-03 01:48:59 -0300

    Added variable positions to check collision

--- libs/collision.lua
+++ libs/collision.lua
@@ -1,5 +1,5 @@
 --[[
-	Version: 0.1.3
+	Version: 0.1.4
 ]]
 
 ---@class Rectangle
@@ -46,19 +46,23 @@ end
 
 ---Checks if the Rectangle is colliding with other object.
 ---@param object Rectangle | Circle
+---@param x? number
+---@param y? number
 ---@return boolean
-function Rectangle:isColliding(object)
+function Rectangle:isColliding(object, x, y)
 	local class = getmetatable(object)
+	x = x or 0
+	y = y or 0
 	assert(class == Rectangle or class == Circle, "Param must be a Rectangle or Circle.")
 
 	if class == Rectangle then
-		return self.x <= object.x + object.width and
-			self.y <= object.y + object.height and
-			self.x + self.width >= object.x and
-			self.y + self.height >= object.y
+		return self.x + x <= object.x + object.width and
+			self.y + y <= object.y + object.height and
+			self.x + self.width + x >= object.x and
+			self.y + self.height + y >= object.y
 	elseif class == Circle then
-		local dx = object.x - math.max(self.x, math.min(object.x, self.x + self.width))
-		local dy = object.y - math.max(self.y, math.min(object.y, self.y + self.height))
+		local dx = object.x - math.max(self.x + x, math.min(object.x, self.x + self.width + x))
+		local dy = object.y - math.max(self.y + y, math.min(object.y, self.y + self.height + y))
 		return math.sqrt(dx ^ 2 + dy ^ 2) <= object.radius
 	end
 	return false
@@ -66,17 +70,21 @@ end
 
 ---Checks if the Circle is colliding with other object.
 ---@param object Rectangle | Circle
+---@param x? number
+---@param y? number
 ---@return boolean
-function Circle:isColliding(object)
+function Circle:isColliding(object, x, y)
 	local class = getmetatable(object)
+	x = x or 0
+	y = y or 0
 	assert(class == Rectangle or class == Circle, "Param must be a Rectangle or Circle.")
 
 	if class == Rectangle then
-		local dx = self.x - math.max(object.x, math.min(self.x, object.x + object.width))
-		local dy = self.y - math.max(object.y, math.min(self.y, object.y + object.height))
+		local dx = self.x + x - math.max(object.x, math.min(self.x + x, object.x + object.width))
+		local dy = self.y + y - math.max(object.y, math.min(self.y + y, object.y + object.height))
 		return math.sqrt(dx ^ 2 + dy ^ 2) <= self.radius
 	elseif class == Circle then
-		return ((object.x - self.x) ^ 2 + (object.y - self.y) ^ 2) ^ 0.5 <= self.radius + object.radius
+		return ((object.x - self.x - x) ^ 2 + (object.y - self.y - y) ^ 2) ^ 0.5 <= self.radius + object.radius
 	end
 	return false
 end
